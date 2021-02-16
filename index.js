35f1b5c0c36103f2ec0a1b49e974d9ecb8459e29
jack <jack.bmiller1@gmail.com>
2021-02-16 20:01:04 +0000

    Add date api

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
     .send('Hello, world!')
     .end();
 });
+
+app.get('/info/api', (req, res) => {
+    const timePassed = Date.now();
+    const today = new Date(timePassed);
+    res.json({'date': today});
+});
  
 // Start the server
 const PORT = process.env.PORT || 8080;
