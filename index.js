3e21afd3e9f4a78b5e99a0c0a6f28ee3fc963e22
jack <jack.bmiller1@gmail.com>
2021-02-16 20:51:50 +0000

    Books!
    Decided to create a book tracker hopefully implementing CRUD into a RESTful api. added index.html, 404.html and GET query.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const app = express();
  
+const Books = [
+    {
+    'id': 1,
+    'name': 'the old man and the sea',
+    'author': 'Hemmingway, Ernest',
+    'publicationDate': 1952,
+    'rating': 8.2},
+
+    {
+    'id': 2,
+    'name': 'Fight Club',
+    'author': 'Palahniuk, Chuck',
+    'publicationDate': 1996,
+    'rating': 9.3},
+];
+
+
 app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello, world!')
-    .end();
+  res.sendFile(__dirname + '/pages/index.html');
 });
 
-app.get('/info/api', (req, res) => {
-    const timePassed = Date.now();
-    const today = new Date(timePassed);
-    res.json({'date': today});
+app.get('/books', (req, res) => {
+   const book = Books.find(c => c.id === parseInt(req.query.id));
+
+   if(!book) res.status(404).sendFile(__dirname + '/pages/404.html');
+
+   else res.send(book);
+        
+   
 });
  
 // Start the server
