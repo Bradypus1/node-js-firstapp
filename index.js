d15201c041437e15a84587536e172577e5b472b4
jack <jack.bmiller1@gmail.com>
2021-02-16 21:54:06 +0000

    Added post functionality
    added the ability to add books to the store

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+
+
+
 const app = express();
- 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const Books = [
     {
     'id': 1,
     'name': 'the old man and the sea',
     'author': 'Hemmingway, Ernest',
-    'publicationDate': 1952,
+    'pDate': 1952,
     'rating': 8.2},
 
     {
     'id': 2,
     'name': 'Fight Club',
     'author': 'Palahniuk, Chuck',
-    'publicationDate': 1996,
+    'pDate': 1996,
     'rating': 9.3},
 ];
 
@@ -29,9 +36,28 @@ app.get('/books', (req, res) => {
 
    else res.send(book);
         
-   
+});
+
+
+app.get('/addbook', (req, res) => {
+    res.sendFile(__dirname + '/pages/books.html');
+});
+
+app.post('/addbook', (req, res) => {
+    
+    const newBook = {
+        'id': Books.length + 1,
+        'name': req.body.name,
+        'author': req.body.author,
+        'pDate': req.body.pDate,
+        'rating': req.body.rating
+    };
+    Books.push(newBook);
+    res.send(newBook);
 });
  
+
+
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
